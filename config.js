58aacd88196ad035544e450290b35746fd54516d
lilhideonstage <85341621+lilhideonstage@users.noreply.github.com>
2021-06-26 20:03:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    introTitle: 'Chị ơi!',
-    introDesc: `Đến đây với em nào đến gần hơn thử xem nào
-    Một mình tít trên cao chị không thấy lẻ loi sao?
-    Chị giờ n'sao chị giờ n'sao
-    Sao không nhanh đến bên nhau`,
+    introTitle: 'Babe à!',
+    introDesc: `Chỉ cần ngồi với em 
+    chút thôi, được không ?
+    Nhìn vào đôi mắt em
+    Thấy sao ? Một thế giới NO HOPE :<`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, em trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
-    btnReply: 'Gửi cho Em <3',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
-    messDesc: 'Khi nào chị muốn thì mình hẹn hò nha.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Alo cho anh bất cứ khi nào em thấy buồn.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://youtu.be/SGlBQR-ftVI'
+    messLink: 'https://m.me/lilhideonstage/'
 }
+```
